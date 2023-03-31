910d2feda346bc75078bea4638dffb4a082bb05f
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-31 14:38:16 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(saturdayActivity = "roller-skate")
+{
+  return(`This Saturday, I want to ${saturdayActivity}!`)
+}
+
+const mondayWork = function(mondayActivity="go to the office"){
+ return(`This Monday, I will ${mondayActivity}.`)
+}
+
+function wrapAdjective(value)
+{
+    return (function(defaultValue = "special")
+    {
+        return(`You are ${value}${defaultValue}${value}!`)
+    })
+}
